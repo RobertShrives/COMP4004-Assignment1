a54c75485f74fd5f09fef159b38b0fc733e7628e
Rob Shrives <robshrives@gmail.com>
2017-10-04 18:13:22 -0400

    Methods added: lookup(String string), testLookupStringPass(), testLookupStringFail()

--- src/UnitTests/TestTitleTable.java
+++ src/UnitTests/TestTitleTable.java
@@ -8,17 +8,28 @@ import server.logic.tables.TitleTable;
 
 public class TestTitleTable {
 
-	TitleTable t1 = new TitleTable();
+	TitleTable titleTable1 = new TitleTable();
 	
 	@Test
 	public void testCreateTitlePass() {
-		assertEquals(true, t1.createtitle("1111111111111", "Test Book"));
+		assertEquals(true, titleTable1.createtitle("1111111111111", "Test Book"));
 	}
 	
 	@Test
 	public void testCreateTitleFail() {
-		t1.createtitle("1111111111111", "Test Book");
-		assertEquals(false, t1.createtitle("1111111111111", "Test Book"));
+		titleTable1.createtitle("1111111111111", "Test Book");
+		assertEquals(false, titleTable1.createtitle("1111111111111", "Test Book"));
+	}
+	
+	@Test
+	public void testLookupStringPass() {
+		titleTable1.createtitle("1111111111111", "Test Book");
+		assertEquals(true, titleTable1.lookup("1111111111111"));
+	}
+	
+	@Test
+	public void testLookupStringFail() {
+		assertEquals(false, titleTable1.lookup("1111111111111"));
 	}
 
 }
