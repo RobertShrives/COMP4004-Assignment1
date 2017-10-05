7e23d1821e5683b355b26cd3b2d8495eb1f600b3
Rob Shrives <robshrives@gmail.com>
2017-10-05 00:56:37 -0400

    Classes added: LoanTable.java, TestLoanTable.java, FeeTable.java
    Methods added: All test cases involed for testing createloan(int i, String string, String string2, Date date)
    
    Note*** The fee table class needed to be added along with numerous functions in loanTable and feeTable just to get this code ina runnning, testable state. For that reason this commit is quite a bit larger than my previous but I assure you I am keeping

---
+++ src/UnitTests/TestLoanTable.java
@@ -0,0 +1,110 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.sun.jmx.snmp.Timestamp;
+
+import server.logic.tables.ItemTable;
+import server.logic.tables.LoanTable;
+import server.logic.tables.TitleTable;
+
+/**
+ * @author robsh
+ *
+ */
+/**
+ * @author robsh
+ *
+ */
+/**
+ * @author robsh
+ *
+ */
+/**
+ * @author robsh
+ *
+ */
+public class TestLoanTable {
+	Timestamp stamp = new Timestamp(System.currentTimeMillis());
+	Date date = new Date(stamp.getDateTime());
+
+	@Test
+	public void testCreateLoanTableSuccessPass() {
+		TitleTable.getInstance().createtitle("9781442","test");
+		ItemTable.getInstance().createitem("9781442");
+		assertEquals("success", LoanTable.getInstance().createloan(4, "9781442", "1", date));
+	}
+	
+	@Test
+	public void testCreateLoanTableSuccessFail() {
+		TitleTable.getInstance().createtitle("9781443","test");
+		ItemTable.getInstance().createitem("9781443");
+		assertNotEquals("The Item is Not Available", LoanTable.getInstance().createloan(4, "9781443", "1", date));
+	}
+	
+	@Test
+	public void testCreateLoanTableMaxPass() {
+		TitleTable.getInstance().createtitle("9781446","test");
+		ItemTable.getInstance().createitem("9781446");
+		TitleTable.getInstance().createtitle("9781447","test1");
+		ItemTable.getInstance().createitem("9781447");
+		TitleTable.getInstance().createtitle("9781448","test2");
+		ItemTable.getInstance().createitem("9781448");
+		TitleTable.getInstance().createtitle("9781449","test3");
+		ItemTable.getInstance().createitem("9781449");
+		LoanTable.getInstance().createloan(4, "9781446", "1", date);
+		LoanTable.getInstance().createloan(4, "9781447", "1", date);	
+		assertEquals("The Maximun Number of Items is Reached", LoanTable.getInstance().createloan(4, "9781448", "1", date));
+	}
+	
+	
+	@Test
+	public void testCreateLoanTableMaxFail() {
+		TitleTable.getInstance().createtitle("9781446","test5");
+		ItemTable.getInstance().createitem("9781446");
+		TitleTable.getInstance().createtitle("9781447","test6");
+		ItemTable.getInstance().createitem("9781447");
+		TitleTable.getInstance().createtitle("9781448","test7");
+		ItemTable.getInstance().createitem("9781448");
+		TitleTable.getInstance().createtitle("9781449","test8");
+		ItemTable.getInstance().createitem("9781449");
+		LoanTable.getInstance().createloan(4, "9781446", "1", date);
+		LoanTable.getInstance().createloan(4, "9781447", "1", date);	
+		assertNotEquals("success", LoanTable.getInstance().createloan(4, "9781448", "1", date));
+	}
+	
+	@Test
+	public void testCreateLoanTableOfeePass() {
+		assertEquals("Outstanding Fee Exists", LoanTable.getInstance().createloan(0, "9781442616899", "1", date));
+	}
+	
+	@Test
+	public void testCreateLoanTableOfeeFail() {
+		assertNotEquals("success", LoanTable.getInstance().createloan(0, "9781442616899", "1", date));
+	}
+	
+	@Test
+	public void testCreateLoanTableNAPass() {
+		TitleTable.getInstance().createtitle("9781442111111","test");
+		ItemTable.getInstance().createitem("9781442111111");
+		LoanTable.getInstance().createloan(1, "9781442111111", "1", date);
+		LoanTable.getInstance().createloan(2, "9781442111111", "1", date);
+		assertEquals("The Item is Not Available", (LoanTable.getInstance().createloan(0, "9781442111111", "1", date)));
+		
+	}
+	
+	@Test
+	public void testCreateLoanTableNAFail() {
+		TitleTable.getInstance().createtitle("9781442111111","test");
+		ItemTable.getInstance().createitem("9781442111111");
+		LoanTable.getInstance().createloan(1, "9781442111111", "1", date);
+		LoanTable.getInstance().createloan(2, "9781442111111", "1", date);
+		assertNotEquals("success", (LoanTable.getInstance().createloan(0, "9781442111111", "1", date)));
+		
+	}
+
+}
---
+++ src/server/logic/tables/LoanTable.java
@@ -0,0 +1,98 @@
+package server.logic.tables;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import server.logic.model.Loan;
+import utilities.Config;
+
+public class LoanTable {
+	
+	List<Loan> loanList=new ArrayList<Loan>();
+    private static class LoanListHolder {
+        private static final LoanTable INSTANCE = new LoanTable();
+    }
+    
+    public static final LoanTable getInstance() {
+        return LoanListHolder.INSTANCE;
+    }
+    
+    public Object createloan(int i, String string, String string2, Date date) {
+		String result="";
+		boolean user=UserTable.getInstance().lookup(i);
+		boolean isbn=TitleTable.getInstance().lookup(string);
+		boolean copynumber=ItemTable.getInstance().lookup(string,string2);
+		boolean oloan=LoanTable.getInstance().lookup(i,string,string2);
+		boolean limit=LoanTable.getInstance().checkLimit(i);
+		boolean fee=FeeTable.getInstance().lookup(i);
+		if(user==false){
+			result="User Invalid";
+			
+		}else if(isbn==false){
+			result="ISBN Invalid";
+			
+		}else if(copynumber==false){
+			result="Copynumber Invalid";
+			
+		}else{
+			if(oloan){
+				if(limit && fee){
+				Loan loan=new Loan(i,string,string2,date,"0");
+				loanList.add(loan);
+				result="success";
+				
+				}else if(limit==false){
+					result="The Maximun Number of Items is Reached";
+					
+				}else if(fee==false){
+					result="Outstanding Fee Exists";
+					
+				}
+			}else{
+				result="The Item is Not Available";
+				
+			}
+		}
+    	return result;
+	}
+    
+    public boolean lookup(int j, String string, String string2) {
+		boolean result=true;
+		int flag=0;
+		for(int i=0;i<loanList.size();i++){
+			String ISBN=(loanList.get(i)).getIsbn();
+			String copynumber=(loanList.get(i)).getCopynumber();
+			if(ISBN.equalsIgnoreCase(string) && copynumber.equalsIgnoreCase(string2)){
+				flag=flag+1;
+			}else{
+				flag=flag+0;	
+			}
+		}
+		if(flag!=0){
+			result=false;
+		}
+		return result;
+	}
+    
+    public boolean checkLimit(int j) {
+		boolean result=true;
+		int flag=0;
+		for(int i=0;i<loanList.size();i++){
+			int userid=(loanList.get(i)).getUserid();
+			if(userid==j){
+				flag=flag+1;
+			}else{
+				flag=flag+0;	
+			}
+		}
+		if(flag>=Config.MAX_BORROWED_ITEMS){
+			result=false;
+		}
+		return result;
+	}
+    
+    public List<Loan> getLoanTable() {
+		return loanList;
+	}
+}
