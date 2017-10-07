ee18bc007d0bbeea93a93a233c6ee0f3332e5c70
Rob Shrives <robshrives@gmail.com>
2017-10-07 01:48:33 -0400

    Classes added: Output.java, TestOutout.java

---
+++ src/UnitTests/TestOutput.java
@@ -0,0 +1,24 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import server.logic.handler.model.Output;
+
+public class TestOutput {
+
+	Output out = new Output("hello", 0);
+	Output dummy;
+	@Test
+	public void testOutputConstructorPass() {
+		assertNotNull(out);
+	}
+	
+	@Test
+	public void testOutputConstructorFail() {
+		assertNull(dummy);
+	}
+
+}
---
+++ src/server/logic/handler/model/Output.java
@@ -0,0 +1,14 @@
+package server.logic.handler.model;
+
+public class Output {
+	String output;
+	int state;
+	
+	public Output(String output,int state){
+		this.output=output;
+		this.state=state;
+		
+	}
+	
+	
+}
