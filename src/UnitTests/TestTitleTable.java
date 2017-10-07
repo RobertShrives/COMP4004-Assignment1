061a96eacf16b28d06619a81e02248841e2f5d4f
Rob Shrives <robshrives@gmail.com>
2017-10-06 21:27:26 -0400

    Methods added: Object delete(String string), testDeleteTitleDoesntExistsPass(), testDeleteTitleDoesntExistsFail()

--- src/UnitTests/TestTitleTable.java
+++ src/UnitTests/TestTitleTable.java
@@ -2,11 +2,15 @@ package UnitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
 
 import org.junit.AfterClass;
 import org.junit.Test;
 
+import server.logic.tables.ItemTable;
+import server.logic.tables.LoanTable;
 import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
 
 public class TestTitleTable {
 
@@ -35,12 +39,53 @@ public class TestTitleTable {
 	@Test
 	public void testGetTitleTablePass() {
 		TitleTable.getInstance().createtitle("4444444444444", "Test Book 4");
-		assertEquals(8, TitleTable.getInstance().getTitleTable().size());
+		assertEquals(9, TitleTable.getInstance().getTitleTable().size());
 	}
 	
 	@Test
 	public void testGetTitleTablefail() {
-		assertNotEquals(9, TitleTable.getInstance().getTitleTable().size());
+		assertNotEquals(8, TitleTable.getInstance().getTitleTable().size());
+	}
+	
+	@Test
+	public void testDeleteSuccessPass() {
+		TitleTable.getInstance().createtitle("7894563214563", "Test");
+		assertEquals("success", TitleTable.getInstance().delete("7894563214563"));
+	}
+	
+	@Test
+	public void testDeleteSuccessFail() {
+		TitleTable.getInstance().createtitle("7894563214563", "Test");
+		assertNotEquals("Active Loan Exists", TitleTable.getInstance().delete("7894563214563"));
+	}
+	
+
+	@Test
+	public void testDeleteLoanExistsPass() {
+		UserTable.getInstance().createuser("Joe@joe", "leto");
+		TitleTable.getInstance().createtitle("7894563213563", "Test");
+		ItemTable.getInstance().createitem("7894563213563");
+		LoanTable.getInstance().createloan(2, "7894563213563", "1", new Date());
+		assertEquals("Active Loan Exists", TitleTable.getInstance().delete("7894563213563"));
+	}
+	
+	@Test
+	public void testDeleteLoanExistsFail() {
+		UserTable.getInstance().createuser("Joe@joe", "leto");
+		TitleTable.getInstance().createtitle("7894563213563", "Test");
+		ItemTable.getInstance().createitem("7894563213563");
+		LoanTable.getInstance().createloan(5, "7894563213563", "1", new Date());
+		assertNotEquals("The Title Does Not Exist", TitleTable.getInstance().delete("7894563213563"));
+	}
+	
+	@Test
+	public void testDeleteTitleDoesntExistsPass() {
+		assertEquals("The Title Does Not Exist", TitleTable.getInstance().delete("111111111"));
+	}
+	
+	@Test
+	public void testDeleteTitleDoesntExistsFail() {
+		assertNotEquals("success", TitleTable.getInstance().delete("111111111"));
 	}
 	
 	@AfterClass
